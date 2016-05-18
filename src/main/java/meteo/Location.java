483d2e1157a30deef2ff9af7695ad84db153d297
DAUTREY <mikael.dautrey@isitix.com>
2016-05-18 14:22:05 +0200

    ajout du modele attache au formulaire de saisie code pays code ville

---
+++ src/main/java/meteo/Location.java
@@ -0,0 +1,13 @@
+package meteo;
+
+/**
+ * Created by mint on 18/05/16.
+ */
+public class Location {
+    private String _codePays;
+    private String _codeVille;
+    public String getCodePays(){return _codePays;}
+    public String getCodeVille(){return _codePays;}
+    public void setCodePays(String codePays){this._codePays = codePays;}
+    public void setCodeVille(String codeVille){this._codeVille = codeVille;}
+}
