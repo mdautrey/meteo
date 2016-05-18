c67d4a40944ddec444c5204eb590d6525042c4f4
DAUTREY <mikael.dautrey@isitix.com>
2016-05-18 14:26:50 +0200

    ajout de la vue

--- src/main/java/meteo/Location.java
+++ src/main/java/meteo/Location.java
@@ -7,7 +7,7 @@ public class Location {
     private String _codePays;
     private String _codeVille;
     public String getCodePays(){return _codePays;}
-    public String getCodeVille(){return _codePays;}
+    public String getCodeVille(){return _codeVille;}
     public void setCodePays(String codePays){this._codePays = codePays;}
     public void setCodeVille(String codeVille){this._codeVille = codeVille;}
 }
