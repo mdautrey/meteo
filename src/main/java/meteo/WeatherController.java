d209611edad1e743d85fb297c403eb04fd56c718
DAUTREY <mikael.dautrey@isitix.com>
2016-05-18 12:56:58 +0200

    Creation du controleur

---
+++ src/main/java/meteo/WeatherController.java
@@ -0,0 +1,18 @@
+package meteo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by mdautrey on 07/10/15.
+ */
+@Controller
+public class WeatherController {
+    @RequestMapping(value="/requestweather", method=RequestMethod.GET)
+    public String requestWeather(Model model) {
+        model.addAttribute("location", new Location());
+        return "requestweather";
+    }
+}
