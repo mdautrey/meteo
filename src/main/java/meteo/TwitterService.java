0169b20527e398db9e24ccd6ccc3cc048c894523
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 16:42:08 +0200

    version aspect OK

--- src/main/java/meteo/TwitterService.java
+++ src/main/java/meteo/TwitterService.java
@@ -1,17 +1,18 @@
 package meteo;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by mint on 19/05/16.
  */
 @Aspect
-@Service
+@Component
 public class TwitterService {
-    @AfterReturning(pointcut = "execution(* getWeather(..))", returning = "retVal")
-    public void sendTweet(Object retVal){
+    @AfterReturning("execution(* getWeatherForecast(..))")
+    public void sendTweet(JoinPoint joinPoint){
         System.err.println("\n\n**********\n\nAspect !\n" +
                 "\n" +
                 "**********\n" +
