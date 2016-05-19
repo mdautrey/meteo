7392fa8c65194b5cb625ac7da94411645be1b3c0
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 09:29:40 +0200

    implementation de l'interface pour obtenir la météo

---
+++ src/main/java/meteo/WeatherService.java
@@ -0,0 +1,24 @@
+package meteo;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by mdautrey on 13/10/15.
+ */
+@Service public class WeatherService implements WeatherInterface {
+
+    private static final String URI = "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s";
+    private static final String APIKey = "752d34ff4a7f1cd4ed111512b0c6276a";
+
+
+    public final WeatherForecast getWeatherForecast(Location location) {
+        //RestTemplate restTemplate = new RestTemplate();
+        //restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        //String url = String.format(URI, location.getCodeVille(), location.getCodePays(), APIKey);
+        //WeatherForecast weatherForecast = restTemplate.getForObject(url, WeatherForecast.class);
+        WeatherForecast weatherForecast = new WeatherForecast();
+        return weatherForecast;
+    }
+}
