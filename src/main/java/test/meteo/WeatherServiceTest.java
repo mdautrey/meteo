ad7e48a8ea26ddf57655cc34f17d01483e4e2ecc
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 12:03:07 +0200

    ajout de tests unitaires

--- src/main/java/test/meteo/WeatherServiceTest.java
+++ src/main/java/test/meteo/WeatherServiceTest.java
@@ -1,40 +1,38 @@
-package test.meteo; 
+package test.meteo;
 
 import meteo.WeatherService;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Before; 
+import org.junit.Before;
 import org.junit.After;
 import meteo.Location;
 
-/** 
-* WeatherService Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>mai 19, 2016</pre> 
-* @version 1.0 
-*/ 
-public class WeatherServiceTest  {
+/**
+ * WeatherService Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>mai 19, 2016</pre>
+ */
+public class WeatherServiceTest {
     Location location;
 
-@Before
-public void before() throws Exception {
-    location = new Location();
-    location.setCodePays("FR");
-    location.setCodeVille("Paris");
-} 
+    @Before
+    public void before() throws Exception {
+        location = new Location();
+        location.setCodePays("FR");
+        location.setCodeVille("Paris");
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-
-@Test(expected = org.springframework.web.client.HttpClientErrorException.class)
-public void testGetWeatherForecast() throws Exception {
-    WeatherService weatherService = new WeatherService();
-    Assert.assertNotNull(weatherService.getWeatherForecast(location));
-    weatherService.setAPIKey("");
-    weatherService.getWeatherForecast(location);
-}
-
+    @Test(expected = org.springframework.web.client.HttpClientErrorException.class)
+    public void testGetWeatherForecast() throws Exception {
+        WeatherService weatherService = new WeatherService();
+        Assert.assertNotNull(weatherService.getWeatherForecast(location));
+        weatherService.setAPIKey("");
+        weatherService.getWeatherForecast(location);
+    }
 } 
