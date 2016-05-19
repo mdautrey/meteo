facb0b07cace06caee88c1350dee2d8c4d3b749d
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 10:40:58 +0200

    ajout des tests unitaires

---
+++ src/main/java/test/meteo/WeatherServiceTest.java
@@ -0,0 +1,35 @@
+package test.meteo; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* WeatherService Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>mai 19, 2016</pre> 
+* @version 1.0 
+*/ 
+public class WeatherServiceTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getWeatherForecast(Location location) 
+* 
+*/ 
+@Test
+public void testGetWeatherForecast() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
