d33595cb9c4bcf84e24525d0cdbd2b42700f5adf
DAUTREY <mikael.dautrey@isitix.com>
2016-05-19 10:24:02 +0200

    activation weathermap

--- src/main/java/meteo/WeatherService.java
+++ src/main/java/meteo/WeatherService.java
@@ -1,6 +1,8 @@
 package meteo;
 
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by mdautrey on 13/10/15.
@@ -8,16 +10,14 @@ import org.springframework.stereotype.Service;
 @Service public class WeatherService implements WeatherInterface {
 
     private static final String URI = "http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s";
-    private static final String APIKey = "752d34ff4a7f1cd4ed111512b0c6276a";
+    private static final String APIKey = "7c4212f5744f459bb53b3b0dbc87a12e";
 
 
     public final WeatherForecast getWeatherForecast(Location location) {
-        //RestTemplate restTemplate = new RestTemplate();
-        //restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        //String url = String.format(URI, location.getCodeVille(), location.getCodePays(), APIKey);
-        //WeatherForecast weatherForecast = restTemplate.getForObject(url, WeatherForecast.class);
-        WeatherForecast weatherForecast = new WeatherForecast();
-        weatherForecast.setName("Test du câblage");
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        String url = String.format(URI, location.getCodeVille(), location.getCodePays(), APIKey);
+        WeatherForecast weatherForecast = restTemplate.getForObject(url, WeatherForecast.class);
         return weatherForecast;
     }
 }
